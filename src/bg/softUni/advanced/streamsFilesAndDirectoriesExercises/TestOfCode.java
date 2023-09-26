b91083a837ff5058c4dda344daf93373f9f2df6c
AntoanYosifov <tapaktapxaomi@gmail.com>
2023-09-27 00:54:02 +0200

    test

--- src/bg/softUni/advanced/streamsFilesAndDirectoriesExercises/TestOfCode.java
+++ src/bg/softUni/advanced/streamsFilesAndDirectoriesExercises/TestOfCode.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class TestOfCode {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("TestGit");
 
 
     }
