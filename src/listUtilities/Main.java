7b1971b7bc3d3c17d13af2a704718c72d91195bd
AntoanYosifov <tapaktapxaomi@gmail.com>
2023-10-15 14:05:34 +0200

    ListUtils

---
+++ src/listUtilities/Main.java
@@ -0,0 +1,14 @@
+package listUtilities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
