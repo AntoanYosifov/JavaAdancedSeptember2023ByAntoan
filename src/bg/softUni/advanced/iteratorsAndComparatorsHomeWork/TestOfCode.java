6d2796f6005d8c53488d6e7548cac671bde81068
AntoanYosifov <tapaktapxaomi@gmail.com>
2023-10-17 06:07:12 +0200

    iteratorsAndComparatorsHomeWork

---
+++ src/bg/softUni/advanced/iteratorsAndComparatorsHomeWork/TestOfCode.java
@@ -0,0 +1,4 @@
+package bg.softUni.advanced.iteratorsAndComparatorsHomeWork;
+
+public class TestOfCode {
+}
---
+++ src/bg/softUni/advanced/iteratorsAndComparatorsHomeWork/strategyPattern/Person.java
@@ -0,0 +1,17 @@
+package bg.softUni.advanced.iteratorsAndComparatorsHomeWork.strategyPattern;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d", name, age);
+    }
+}
